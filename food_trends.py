c607997eaa896542ffed03d923f8c813ff3e4b88
Kasey Cox <ka.cox@outlook.com>
2018-08-07 23:37:05 +0000

    Get app to connect to db

--- food_trends.py
+++ food_trends.py
@@ -4,3 +4,26 @@ Food Trends app made with Flask.
 This file creates and configures the Flask application instance and connects 
 to the database.
 """
+
+# imports
+from flask import Flask
+
+from db import connect_to_db
+
+
+# create app instance
+app = Flask(__name__)
+
+#####################################################################
+# routes & view functions
+@app.route("/")
+def index():
+    return "<h1>Food Trends app</h1>"
+
+#####################################################################
+# connect to db
+connect_to_db(app)
+print("Connected to DB")
+
+# run application on localhost
+app.run(port=5000, host='0.0.0.0')
