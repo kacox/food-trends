b9599a8d6d940d31d0fe8a348c2c279c5ea56294
Kasey Cox <ka.cox@outlook.com>
2018-08-07 21:11:34 +0000

    Initial commit

---
+++ db.py
@@ -0,0 +1,1 @@
+"""Database functions for Food Trends application."""
---
+++ food_trends.py
@@ -0,0 +1,6 @@
+"""
+Food Trends app made with Flask.
+
+This file creates and configures the Flask application instance and connects 
+to the database.
+"""
