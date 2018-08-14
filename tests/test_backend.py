b3d4c09e33d541063dae071c159c3c517872b571
Kasey Cox <ka.cox@outlook.com>
2018-08-14 20:11:12 +0000

    Change test for handling Spoonacular's POST Detect Food in Text endpoint
    Rewrite pytest unit test as unittest TestCase class (TestFirstApiCall).
    Refactor testing code for the above reason.

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -3,19 +3,47 @@
 # imports
 import sys
 import pytest
+import unittest
 
 # import from parent directory
 sys.path.append("/home/vagrant/src/food_trends_app/")
 import calls
 
 
-# skip unless you specifically want to run
-@pytest.mark.skip(reason="Limited number of API calls allowed.")
-def test_get_food_terms():
-    """Should get food terms from input text."""
-    input1 = "Low - Carb Banana Pancakes"
-    assert "banana" in calls.get_food_terms(input1, calls.MASHAPE_KEY)
-    assert "pancakes" in calls.get_food_terms(input1, calls.MASHAPE_KEY)
+class TestFirstApiCall(unittest.TestCase):
+    """
+    Test first interaction with Spoonacular's POST Detect Food in Text 
+    endpoint.
+    """
+
+    def setUp(self):
+        """Mock API call before each test."""
+
+        # Make mock
+        def _mock_get_food_terms(input_text, api_key):
+            return {'annotations': 
+                        [{'annotation': 'banana cake', 'tag': 'dish', 
+                            'image': 'https://spoonacular.com/menuItemImages/plastic-to-go-drink-cup.jpg'}, 
+                        {'annotation': 'banana', 'tag': 'ingredient', 
+                            'image': 'https://spoonacular.com/cdn/ingredients_100x100/bananas.jpg'}, 
+                        {'annotation': 'cake', 'tag': 'dish', 
+                            'image': 'https://spoonacular.com/menuItemImages/plastic-to-go-drink-cup.jpg'}], 
+                    'processedInMs': 9}
+
+        # Monkey patching
+        calls.get_food_terms = _mock_get_food_terms
+
+    def test_terms_from_response(self):
+        """Should extract food terms from JSON response."""
+        # mock API call
+        input_text = "banana cake is good"
+        api_key = calls.MASHAPE_KEY
+        response_content = calls.get_food_terms(input_text, api_key)
+
+        # tests
+        assert 'banana cake' in calls.terms_from_response(response_content)
+        assert 'banana' in calls.terms_from_response(response_content)
+        assert 'cake' in calls.terms_from_response(response_content)
 
 
 def test_get_search_window():
@@ -33,18 +61,18 @@ def test_build_twingly_query():
             "banana fields:title lang:en page-size:3 sort:created tspan:w")
 
 
-def test_terms_from_response():
-    """Should extract food terms from JSON response."""
-    # example of what response contains as a Python object
-    test_response_content = {'annotations': 
-                                [{'annotation': 'banana cake', 'tag': 'dish', 
-                                'image': 'https://spoonacular.com/menuItemImages/plastic-to-go-drink-cup.jpg'}, 
-                                {'annotation': 'banana', 'tag': 'ingredient', 
-                                'image': 'https://spoonacular.com/cdn/ingredients_100x100/bananas.jpg'}, 
-                                {'annotation': 'cake', 'tag': 'dish', 
-                                'image': 'https://spoonacular.com/menuItemImages/plastic-to-go-drink-cup.jpg'}], 
-                             'processedInMs': 9}
-    # no guaranteed order, so "assert ___ in" necessary
-    assert 'banana cake' in calls.terms_from_response(test_response_content)
-    assert 'banana' in calls.terms_from_response(test_response_content)
-    assert 'cake' in calls.terms_from_response(test_response_content)
+"""
+Do not run unless you specifically want to; makes an actual API call 
+and tests that I handle the response correctly.
+"""
+@pytest.mark.skip(reason="Limited number of API calls allowed.")
+def test_get_food_terms():
+    """Should get food terms from input text."""
+    input1 = "Low - Carb Banana Pancakes"
+    assert "banana" in calls.get_food_terms(input1, calls.MASHAPE_KEY)
+    assert "pancakes" in calls.get_food_terms(input1, calls.MASHAPE_KEY)
+
+
+if __name__ == '__main__':
+    """Do unittest tests if run directly."""
+    unittest.main()
