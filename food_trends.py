b04e75394e70d2ac675ee28fc2575f3160396256
Kasey Cox <ka.cox@outlook.com>
2018-08-27 20:06:43 +0000

    Code refactor: route structure and flow

--- food_trends.py
+++ food_trends.py
@@ -5,44 +5,32 @@ This file creates and configures the Flask application instance and connects
 to the database.
 """
 
-# imports
+import os
+import json
+
 from flask import Flask, render_template, redirect, request, url_for, flash
 from flask import jsonify
-
-import os
 
 import calls
 import forms
 
 
-# get secret key
 APP_KEY = os.environ.get("APP_KEY")
-
-# create app instance
 app = Flask(__name__)
-
-# session enabling and CSRF protection
 app.config['SECRET_KEY'] = APP_KEY
 
-
 #####################################################################
-# routes & view functions
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """Display landing page with search form."""
+
     form = forms.QueryForm(request.form)
 
     if form.validate_on_submit():
-        # Successful POST request; take data
         query = form.user_query.data
-
-        # give results to backend function (for now, display placeholder)
-        # second arg for url_for is sent as GET request
-        return redirect(url_for("display_search", srch_query=query))
+        return redirect(url_for("get_final_term", srch_query=query))
         
-
-    # Form not submitted; no data; back to original page
     if form.errors:
-        # if there are any validation errors
         for error in form.errors["user_query"]:
             flash(error)
 
@@ -50,14 +38,15 @@ def index():
 
 
 @app.route("/verify-term", methods=["GET", "POST"])
-def display_search():
-    # get the user's query string
+def get_final_term():
+    """Get the final search term from the user query."""
+
     query = request.args.get("srch_query")
 
     # make request to Spoonacular (find food terms in user query)
     parsed_terms = calls.get_food_terms(query, calls.MASHAPE_KEY)
 
-    # if more than one let user pick
+    # want user to pick the final search term
     if len(parsed_terms) > 1:
         return render_template("term_chooser.html", 
                                 header_text=query, 
@@ -68,21 +57,38 @@ def display_search():
 
 @app.route("/search", methods=["GET", "POST"])
 def search_blogs():
-    # get final term depending on request type
+    """Do blog search with final search term."""
+
     if request.method == "GET":
         final_term = request.args.get("choice")
     else:
-        # query had more than one choice; narrowed to one now
         final_term = request.form.get("choice")
 
-    # hard-coded to "carrot" right now; change to final_term later
-    results_dict = jsonify(calls.find_matches())
-    return results_dict
+    # `final_term` hard-coded to "carrot" right now; change later
+    results_dict = jsonify(calls.find_matches()).data
+
+    return redirect(url_for("display_results", 
+                                srch_results=results_dict, 
+                                srch_term="carrot"))
+
+
+@app.route("/results", methods=["GET"])
+def display_results():
+    """Show the search results and calculated metrics."""
+
+    srch_term = request.args.get("srch_term")
+    results = json.loads(request.args.get("srch_results"))
+    
+    # calculate search term popularity
+    # for each pairing, calculate pairing popularity
+
+    return render_template("results.html", 
+                            header_text=srch_term, 
+                            results=list(results.keys()))
 
 
 #####################################################################
 
 if __name__ == "__main__":
     """Run the server."""
-    # run application on localhost
     app.run(port=5000, host="0.0.0.0", debug=True)
